92212c8bdb4e052bb04580d2af1d95699fffa09c
Victor Antonio Hernandez Nuñez <victor.hernandezn@outlook.com>
2024-02-09 19:39:04 +0100

    remove aproximation from focal length calculation

--- vue/src/helpers/focal-length.helper.ts
+++ vue/src/helpers/focal-length.helper.ts
@@ -1,4 +1,5 @@
 import type { LensSurfaceType } from "@/types/configuration.type"
+import { computed } from "vue"
 
 type LensProps = {
   radius?: number,
@@ -16,16 +17,12 @@ export default {
   ): number {
     if (front.radius === undefined || back.radius === undefined) return 0
 
-    const signedFrontRadius = (front.surface === "convex" ? 1 : -1) * front.radius
-    const signedBackRadius = (back.surface === "convex" ? -1 : 1) * back.radius
-    const frontRadiusReciprocal = Math.ceil((1 / signedFrontRadius) * 100) / 100
-    const backRadiusReciprocal = Math.ceil((1 / signedBackRadius) * 100) / 100
-    
+    const signedFrontRadius = computed(() => (front.surface === "convex" ? 1 : -1) * (front.radius ?? 0))
+    const signedBackRadius = computed(() => (back.surface === "convex" ? -1 : 1) * (back.radius ?? 0))
+    const frontRadiusReciprocal = computed(() => 1 / signedFrontRadius.value)
+    const backRadiusReciprocal = computed(() => 1 / signedBackRadius.value)
+
     // 1 / f = (n - 1) * (1 / R1 - 1 / R2 + (n - 1) * d / (n * R1 * R2)
-    if (thickness === 0) {
-      return 1 / ((refractionIndex - 1) * (frontRadiusReciprocal - backRadiusReciprocal))
-    } else {
-      return 1 / ((refractionIndex - 1) * (frontRadiusReciprocal - backRadiusReciprocal + ((refractionIndex - 1) * thickness) / (refractionIndex * signedFrontRadius * signedBackRadius)))
-    }
+    return 1 / ((refractionIndex - 1) * (frontRadiusReciprocal.value - backRadiusReciprocal.value + ((refractionIndex - 1) * thickness) / (refractionIndex * signedFrontRadius.value * signedBackRadius.value)))
   }
 }
