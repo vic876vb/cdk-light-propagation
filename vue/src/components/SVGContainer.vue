2ff6d1de8e2d8807e36ede3f00554ca3e40387b8
Victor Antonio Hernandez Nuñez <victor.hernandezn@outlook.com>
2024-02-09 18:22:00 +0100

    read and store svg canvas dimensions

--- vue/src/components/SVGContainer.vue
+++ vue/src/components/SVGContainer.vue
@@ -1,5 +1,6 @@
 <template>
   <svg
+    ref="svgContainerRef"
     class="w-full min-h-[300px]"
     version="1.1"
     id="svg10"
@@ -8,3 +9,20 @@
     <slot></slot>
   </svg>
 </template>
+
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import useControlsStore from '@/stores/controls.store'
+
+  const store = useControlsStore()
+  const svgContainerRef = ref<HTMLElement>()
+
+  onMounted(() => {
+    if (!svgContainerRef.value) return
+
+    store.$patch({
+      canvasWidth: svgContainerRef.value?.clientWidth,
+      canvasHeight: svgContainerRef.value?.clientHeight,
+    })
+  })
+</script>@/stores/controls.store
